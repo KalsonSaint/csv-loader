ede507ba99f267e56fd343d6cf96f2d6cc2f85a6
KalsonSaint <saintkalson@gmail.com>
2020-10-23 16:09:53 +0100

    Parsing Number Values

--- load-csv.js
+++ load-csv.js
@@ -5,6 +5,19 @@ const loadCSV = (filename, options) => {
 	let data = fs.readFileSync(filename, {encoding:'utf8'});
 	data = data.split('\n').map(row => row.split(','));
 	data = data.map(row => _.dropRightWhile(row, val => val === ''))
+	const headers = _.first(data);
+
+	data = data.map((row, index) => {
+		if(index === 0){
+			return row;
+		}
+
+		return row.map((element, index) => {
+			const result = parseFloat(element);
+			return _.isNaN(result) ? element : result;
+		})
+	})
+
 	console.log(data)
 }
 
