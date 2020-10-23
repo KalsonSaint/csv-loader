f3cd20fca0e3a400005327939569516cd72c51e6
KalsonSaint <saintkalson@gmail.com>
2020-10-23 16:02:31 +0100

    Dropping trailing columns

--- load-csv.js
+++ load-csv.js
@@ -4,7 +4,7 @@ const _ = require('lodash')
 const loadCSV = (filename, options) => {
 	let data = fs.readFileSync(filename, {encoding:'utf8'});
 	data = data.split('\n').map(row => row.split(','));
-	data.map(row => _.dropRightWhile(row, val => val === ''))
+	data = data.map(row => _.dropRightWhile(row, val => val === ''))
 	console.log(data)
 }
 
